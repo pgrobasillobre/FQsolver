07b405795577abd3a7c13ba7a1e71e00c615e3f2
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2026-04-08 11:16:46 +0200

    build elimination

--- build/omp_stub/omp.h
+++
@@ -1,6 +0,0 @@
-// Auto-generated shim used when ENABLE_OMP=OFF
-#ifndef FQSolver_OMP_STUB_H
-#define FQSolver_OMP_STUB_H
-static inline int omp_get_max_threads() { return 1; }
-static inline int omp_get_thread_num()  { return 0; }
-#endif // FQSolver_OMP_STUB_H
